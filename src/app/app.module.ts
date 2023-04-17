5f03e32a9865df0e6ccb04156483ef68b230c49d
ARRakshitha <rakshithaar878@gmail.com>
2023-04-17 14:01:25 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+import { UserComponent } from './user/user.component';
+import { UserlistComponent } from './userlist/userlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    MainDashboardComponent,
+    UserComponent,
+    UserlistComponent
   ],
   imports: [
     BrowserModule,
